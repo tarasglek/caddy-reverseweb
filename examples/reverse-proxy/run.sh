0e84cdfae718c6577e3a42433362585643dca2db
Taras Glek <taras@glek.net>
2026-01-25 10:43:12 +0200

    feat: use air for hot reloading in reverse-proxy example

--- examples/reverse-proxy/run.sh
+++ examples/reverse-proxy/run.sh
@@ -2,7 +2,10 @@
 set -ex
 
 # Change to the directory of this script
-cd "$(dirname "$0")"
+CONFIG_DIR=$(dirname "$0")
+pushd $CONFIG_DIR/../..
+
+air="go run github.com/air-verse/air@v1.64.4"
 
 # Call the central run script
-../../cmd/caddy/run.sh caddy.config
+$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/caddy.config"
