19beb2159d4d86ab17f56e5bb5d72ba4d2daa5c9
Taras Glek <taras@glek.net>
2026-01-25 15:12:43 +0200

    misc

--- examples/reverse-proxy/run.sh
+++ examples/reverse-proxy/run.sh
@@ -8,4 +8,4 @@ pushd $CONFIG_DIR/../..
 air="go run github.com/air-verse/air@v1.64.4"
 
 # Call the central run script
-$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile"
+$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,json" --build.include_file /home/taras/Documents/caddy-reverseweb/examples/reverse-proxy/Caddyfile -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile" -d
