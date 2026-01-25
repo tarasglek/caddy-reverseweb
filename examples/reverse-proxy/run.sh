20d9f889dcb27404b559a81911c6a048c03c873d
Taras Glek <taras@glek.net>
2026-01-25 12:07:02 +0200

    refactor: rename caddy.config to Caddyfile in reverse-proxy example

--- examples/reverse-proxy/run.sh
+++ examples/reverse-proxy/run.sh
@@ -8,4 +8,4 @@ pushd $CONFIG_DIR/../..
 air="go run github.com/air-verse/air@v1.64.4"
 
 # Call the central run script
-$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/caddy.config"
+$air --build.entrypoint "./tmp/caddy"  --build.cmd "go build -o ./tmp/caddy cmd/caddy/main.go" -build.include_ext "go,config,Caddyfile" -build.args_bin "run --adapter caddyfile --config $CONFIG_DIR/Caddyfile"
