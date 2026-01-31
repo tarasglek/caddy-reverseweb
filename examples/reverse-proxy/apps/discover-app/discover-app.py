4adb6900a6013d676e8507bdc1b00d0b7395bfaa
Taras Glek <taras@glek.net>
2026-01-31 15:18:22 +0200

    fix: update detector path and proxy port format in reverse-proxy example

--- examples/reverse-proxy/apps/discover-app/discover-app.py
+++ examples/reverse-proxy/apps/discover-app/discover-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import json
 print(
-json.dumps({"executable":["python3", "-m", "http.server", "23232"], "reverse_proxy_to":"23232"})
+json.dumps({"executable":["python3", "-m", "http.server", "23232"], "reverse_proxy_to":":23232"})
 )
